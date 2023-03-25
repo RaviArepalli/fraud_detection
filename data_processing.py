dff01e0a5f9e13869a6780cf5197add8f9b87ee0
ravi <ravibabuarepalli@gmail.com>
2023-03-25 17:10:08 +0530

    modified file

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,17 @@
 
 print("Adding all the data processing code related to fruad detection")
+
+a = 5
+b=7
+
+def sub(a,b):
+    c = a+b
+    return c
+
+print("Sum function")
+
+def mul(a,b):
+    c = a*b
+    return b
+
+print("mul function")
