393e974425f80d1e7c122576cf6675ba88e3d9d2
ravi <ravibabuarepalli@gmail.com>
2023-03-25 17:05:59 +0530

    Revert "modified file"
    This reverts commit 81bdc4dd2a6f9db9a146e59e7837c8a3c5194cc4.

--- data_processing.py
+++ data_processing.py
@@ -1,11 +1,2 @@
 
 print("Adding all the data processing code related to fruad detection")
-
-a =5
-b= 7
-
-def sub(a,b):
-    c=a+b
-    return code
-
-print ("Add a new function sum which is not correct")
